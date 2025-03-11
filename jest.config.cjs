9696115339b0a3cb7d4e3c1730b44c602bdf5b12
alexanimal <dcalex30@gmail.com>
2025-03-11 08:43:35 -0700

    chore: Update Jest configuration to use default TypeScript config

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { 
-      tsconfig: 'tsconfig.test.json',
+      tsconfig: 'tsconfig.json',
       useESM: true
     }]
   },
